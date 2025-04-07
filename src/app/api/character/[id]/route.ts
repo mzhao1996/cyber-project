80a3423c196b380d28ed65e3d96a9aaf7acd0e99
mzhao1996 <106331724+mzhao1996@users.noreply.github.com>
2025-04-07 10:27:51 -0400

    for deploy vercel

--- src/app/api/character/[id]/route.ts
+++ src/app/api/character/[id]/route.ts
@@ -3,16 +3,22 @@ import fs from 'fs';
 import path from 'path';
 import { Character } from '@/type/character';
 
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
 export async function GET(
   request: Request,
-  context: { params: { id: string } }
+  { params }: Props
 ) {
   try {
     const filePath = path.join(process.cwd(), 'src/data/Character.json');
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const characterData: Character[] = JSON.parse(fileContent);
     
-    const character = characterData.find((char: Character) => char.id === context.params.id);
+    const character = characterData.find((char: Character) => char.id === params.id);
     
     if (!character) {
       return NextResponse.json(
